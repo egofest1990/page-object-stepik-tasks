6ab65a2faa2ae825941c2482fd5668e99a790296
Ihor Fesenko <egofest1990@gmail.com>
2020-04-09 14:34:50 +0300

    added a number of tests and checks, Task: adding to the basket from the product page, Task: data independence, Task: content independence, looking for a bug, Task: negative checks

--- pages/locators.py
+++ pages/locators.py
@@ -14,3 +14,14 @@ class LoginPageLocators():
     REGISTER_PASS1 = (By.CSS_SELECTOR, "#id_registration-password1")
     REGISTER_PASS2 = (By.CSS_SELECTOR, "#id_registration-password2")
     REGISTER_BUTTON = (By.CSS_SELECTOR, "[name='registration_submit']")
+
+
+class ProductPageLocators():
+    ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    BOOK_NAME = (By.XPATH, "//h1")
+    BOOK_NAME_IN_BASKET = (By.CSS_SELECTOR, "#messages .alert-success:nth-child(1) > .alertinner > strong")
+    BOOK_NAME_ALERT = (By.XPATH, "//div[2]//div/div[1]/div/strong")
+    BASKETS_PRICE = (By.CSS_SELECTOR, ".alert-info .alertinner p strong")
+    BOOK_PRICE_IN_MSG = (By.CSS_SELECTOR,".basket-mini")
+    BOOK_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages .alert-success:nth-child(1) > .alertinner")
