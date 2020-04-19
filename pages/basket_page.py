55826924554903325894521f6582a465f5f2ece8
Ihor Fesenko <egofest1990@gmail.com>
2020-04-19 22:46:16 +0300

    Using Inheritance. Methods that can be used from different pages have been transferred to BasePage

---
+++ pages/basket_page.py
@@ -0,0 +1,13 @@
+from .base_page import BasePage
+from .locators import BasketPageLocators
+
+
+class BasketPage(BasePage):
+    def should_be_empty_baskets_mwssage(self):
+        basket_message = self.browser.find_element(*BasketPageLocators.BASKET_MESSAGE).text
+        empty_message = "Your basket is empty."
+        assert basket_message.find(empty_message) != -1, "No empty message. but it should be"
+
+    def should_not_be_items_in_basket(self):
+        assert self.is_not_element_present(*BasketPageLocators.BASKET_ITEMS) == True, \
+            "Items in basket are presented, but should not be"
--- pages/locators.py
+++ pages/locators.py
@@ -30,3 +30,9 @@ class ProductPageLocators():
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    VIEW_BASKET = (By.CSS_SELECTOR, ".basket-mini a.btn")
+
+
+class BasketPageLocators():
+    BASKET_MESSAGE = (By.CSS_SELECTOR, "#content_inner p")
+    BASKET_ITEMS = (By.CSS_SELECTOR, ".basket-items")
